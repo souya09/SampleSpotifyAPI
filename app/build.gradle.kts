fe4cebe792524695e6f272f38b5d680cc8ade91a
cmstudent <22cm0107.jec.ac.jp>
2023-10-04 11:32:27 +0900

    ・。・

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,12 +29,16 @@ android {
 }
 
 dependencies {
+    implementation ("com.spotify.android:auth:1.2.5") // Maven dependency
+    implementation ("com.google.code.gson:gson:2.8.6")
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation(files("/Users/cmstudent/aar/spotify-app-remote-release-0.8.0.aar"))
+    implementation(files("libs/spotify-app-remote-release-0.8.0.aar"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+
 }
--- app/src/main/java/jp/ac/hec/cm0107/samplespotifyapi/MainActivity.java
+++ app/src/main/java/jp/ac/hec/cm0107/samplespotifyapi/MainActivity.java
@@ -3,9 +3,21 @@ package jp.ac.hec.cm0107.samplespotifyapi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+
+import com.spotify.protocol.client.Subscription;
+import com.spotify.protocol.types.PlayerState;
+import com.spotify.protocol.types.Track;
 
 
 public class MainActivity extends AppCompatActivity {
+    private static final String CLIENT_ID = "304b769601404fb3a87f98ca093f9bf9";
+    private static final String REDIRECT_URI = "http://22cm0107.main.jp/SpotifySDK-redirection";
+    private SpotifyAppRemote mSpotifyAppRemote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,16 +28,50 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        // We will start writing our code here.
-    }
+        ConnectionParams connectionParams =
+                new ConnectionParams.Builder(CLIENT_ID)
+                        .setRedirectUri(REDIRECT_URI)
+                        .showAuthView(true)
+                        .build();
 
-    private void connected() {
-        // Then we will write some more code here.
+        SpotifyAppRemote.connect(this, connectionParams,
+                new Connector.ConnectionListener() {
+
+                    public void onConnected(SpotifyAppRemote spotifyAppRemote) {
+                        mSpotifyAppRemote = spotifyAppRemote;
+                        Log.d("MainActivity", "Connected! Yay!");
+
+                        // Now you can start interacting with App Remote
+                        connected();
+
+                    }
+
+                    public void onFailure(Throwable throwable) {
+                        Log.e("MyActivity", throwable.getMessage(), throwable);
+
+                        // Something went wrong when attempting to connect! Handle errors here
+                    }
+                });
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        // Aaand we will finish off here.
+        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
+    }
+
+    private void connected() {
+        // Play a playlist
+        mSpotifyAppRemote.getPlayerApi().play("spotify:playlist:37i9dQZF1DX2sUQwD7tbmL");
+
+        // Subscribe to PlayerState
+        mSpotifyAppRemote.getPlayerApi()
+                .subscribeToPlayerState()
+                .setEventCallback(playerState -> {
+                    final Track track = playerState.track;
+                    if (track != null) {
+                        Log.d("MainActivity", track.name + " by " + track.artist.name);
+                    }
+                });
     }
 }
